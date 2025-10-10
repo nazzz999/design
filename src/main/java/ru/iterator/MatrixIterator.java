1a9063c49739787c9424dc4f6dd663998e22b3b1
nazarnass <nazarychevaanastacia@yandex.ru>
2025-10-10 16:29:21 +0300

    27_2_Итератор_для_двухмерного_массива_int

---
+++ src/main/java/ru/iterator/MatrixIterator.java
@@ -0,0 +1,36 @@
+package ru.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIterator implements Iterator<Integer> {
+    private final int[][] data;
+    private int row;
+    private int column;
+
+    public MatrixIterator(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (row < data.length && (data[row] == null
+                || column >= data[row].length)) {
+            row++;
+            column = 0;
+        }
+        return row < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (column >= data[row].length) {
+            row++;
+            column = 0;
+        }
+        return data[row][column++];
+    }
+}
