b12877c97d2f7c17551641249aee8147ecd38200
nazarnass <nazarychevaanastacia@yandex.ru>
2025-10-11 09:06:40 +0300

    27.3 Итератор четных чисел

---
+++ src/main/java/ru/iterator/EvenNumbersIterator.java
@@ -0,0 +1,30 @@
+package ru.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (index <= data.length - 1 && data[index] % 2 != 0) {
+            index++;
+        }
+        return index <= data.length - 1;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[index++];
+    }
+}
