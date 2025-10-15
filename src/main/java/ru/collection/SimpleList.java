88d88020e9bf10ee98abb4e59924e3a22a4e1e64
nazarnass <nazarychevaanastacia@yandex.ru>
2025-10-15 21:19:39 +0300

    29_4_Динамический_список_на_массиве

--- src/main/java/ru/collection/SimpleList.java
+++ src/main/java/ru/collection/SimpleList.java
@@ -2,8 +2,13 @@ package ru.collection;
 
 public interface SimpleList<T> extends Iterable<T> {
     void add(T value);
+
     T set(int index, T newValue);
+
     T remove(int index);
+
     T get(int index);
+
     int size();
+
 }
