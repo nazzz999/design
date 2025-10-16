de76f39f6885cda7b5a34f4f324034bbf5599e38
nazarnass <nazarychevaanastacia@yandex.ru>
2025-10-16 17:02:31 +0300

    29_4_Динамический_список_на_массиве

---
+++ src/main/java/ru/collection/SimpleLinked.java
@@ -0,0 +1,6 @@
+package ru.collection;
+
+public interface SimpleLinked<E> extends Iterable<E> {
+    void add(E value);
+    E get(int index);
+}
---
+++ src/main/java/ru/collection/SimpleLinkedList.java
@@ -0,0 +1,79 @@
+package ru.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements SimpleLinked<E> {
+
+    private int size;
+    private int modCount;
+    private Node<E> head;
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> current = head;
+            private final int expectedModCount = modCount;
+
+            private void checkForCoModification() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                checkForCoModification();
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E value = current.item;
+                current = current.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
