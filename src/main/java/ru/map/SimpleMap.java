bada08d2e7d2c9ed9fb4e1f6f7df57ec5e9631e8
nazarnass <nazarychevaanastacia@yandex.ru>
2025-11-06 16:59:43 +0300

    31.10 Реализовать собственную HashMap

--- src/main/java/ru/map/SimpleMap.java
+++ src/main/java/ru/map/SimpleMap.java
@@ -1,8 +1,11 @@
 package ru.map;
 
 public interface SimpleMap<K, V> extends Iterable<K> {
-    boolean put(K key, V value);
-    V get(K key);
-    boolean remove(K key);
-}
 
+    boolean put(K key, V value);
+
+    V get(K key);
+
+    boolean remove(K key);
+
+}
