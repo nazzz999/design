dce9ae0c93caadf0ed87dd5411a349db1b0309a2
nazarnass <nazarychevaanastacia@yandex.ru>
2025-11-13 19:27:09 +0300

    32_2_Создать_элементарную_структуру_дерева

---
+++ src/main/java/ru/tree/SimpleTree.java
@@ -0,0 +1,37 @@
+package ru.tree;
+
+import java.util.*;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> resultParent = findBy(parent);
+        if (resultParent.isEmpty() || findBy(child).isPresent()) {
+            return false;
+        }
+        resultParent.get().children.add(new Node<>(child));
+        return true;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> element = data.poll();
+            if (element.value.equals(value)) {
+                result = Optional.of(element);
+                break;
+            }
+            data.addAll(element.children);
+        }
+        return result;
+    }
+}
