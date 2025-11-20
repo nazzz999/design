50323d7fd9d340285c549a7626683a582a67da65
nazarnass <nazarychevaanastacia@yandex.ru>
2025-11-20 17:15:49 +0300

    33.3 FileOutputStream

---
+++ src/main/java/ru/io/ResultFile.java
@@ -0,0 +1,31 @@
+package ru.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        int size = 10;
+
+        int[][] multiplicationTable = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                multiplicationTable[i][j] = (i + 1) * (j + 1);
+            }
+        }
+
+
+        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    String line = String.format("%4d", multiplicationTable[i][j]);
+                    output.write(line.getBytes());
+                }
+                output.write(System.lineSeparator().getBytes());
+            }
+            System.out.println("Таблица умножения успешно записана в файл.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
